4d6c4f92f413333429de27fd4153c27e5d77a556
ESHAN SHETTENNAVAR <112294700+duckhitches@users.noreply.github.com>
2024-12-19 13:45:02 +0545

    Update firebaseConfig.ts

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDiH_6Iju6fr-QE7cmnsrSq0QFPIRsLQPI",
-    authDomain: "ecommmerce-90273.firebaseapp.com",
-    projectId: "ecommmerce-90273",
-    storageBucket: "ecommmerce-90273.firebasestorage.app",
-    messagingSenderId: "952259731226",
-    appId: "1:952259731226:web:f04c55749a77f82ccd56a3",
+ 
     // measurementId: "G-67DPBDYWTJ"
 };
 
